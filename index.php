62fe869d2c3ecdad7bd06244f3a0ded31ff4a5fa
1414504 <1414504@rgu.ac.uk>
2016-08-17 13:55:47 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+
+    <link href="bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <title>My First Project</title>
+
+</head>
+
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-3" style="height: 140px">
+            <img src="image/rose.jpg" style="height: 140px">
+        </div>
+        <div class="col-lg-9" style="height: 140px">
+            <h2>I Love Pakistan</h2>
+            <h3>I Love my country very much</h3>
+        </div>
+
+    </div>
+</div>
+
+</body>
+
+</html>
